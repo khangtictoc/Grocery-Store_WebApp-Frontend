68d585dbbb7aa5553bf9aefd34ea3d509e3fb657
khangtictoc <tranhoangkhang09112001@gmail.com>
2024-02-13 01:04:27 +0700

    Display category done UI

--- js/authentication/test.js
+++
@@ -1,7 +0,0 @@
-fetch('http://localhost:9092/user/logout', {
-    method: 'POST',
-    mode: 'cors',
-    headers: {
-        "Authorization" : "Bearer " + localStorage.getItem("accessToken")
-    }
-}).then(response => {}).then(data => { console.log(data); }).catch(error => console.error('Error:', error));
---
+++ js/category/loadItem.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $.ajax({
+        url: "http://localhost:9092/category/getall",
+        method: "GET",
+        crossDomain: true
+    }).done(function(response){
+        console.log("server tra ve ", response)
+        const path = "//*[@id='bs-megadropdown-tabs']/ul"
+        const categoryLst = document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+        categoryLst.innerHTML=''
+
+        const dataResult = response.data;
+        for (let i = 0; i < dataResult.length; i++) {
+            var data = dataResult[i];
+            var divElement = document.createElement('li');
+            divElement.innerHTML =
+            `
+                <a href="#">${data.categoryName}</a>
+            `;
+            categoryLst.appendChild(divElement);
+        }
+    })
+})
